186f4c7d040b8a37617e1f914b337ae7216e892c
DESKTOP-9CAQJI2\zst <zhangst5784@gmail.com>
2024-04-27 17:09:22 +0800

    feat: 接口调整

--- registry-server/src/main/java/com/zst/registrycenter/transport/RegistryController.java
+++ registry-server/src/main/java/com/zst/registrycenter/transport/RegistryController.java
@@ -7,12 +7,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Slf4j
@@ -22,18 +25,18 @@ public class RegistryController {
     @Autowired
     private RegistryService registryService;
 
-    @GetMapping("/getInstances")
-    public List<InstanceMetadata> getInstances(@RequestParam("serviceId") String serviceId) {
+    @GetMapping("/getInstances/{serviceId}")
+    public List<InstanceMetadata> getInstances(@PathVariable("serviceId") String serviceId) {
         return registryService.getAllInstances(serviceId);
     }
 
-    @GetMapping("/version")
-    public Long getVersion(@RequestParam("serviceId") String serviceId) {
+    @GetMapping("/version/{serviceId}")
+    public Long getVersion(@PathVariable("serviceId") String serviceId) {
         return registryService.getVersion(serviceId);
     }
 
-    @PutMapping("/register")
-    public void register(@RequestParam("serviceId") String serviceId,
+    @PutMapping("/register/{serviceId}")
+    public void register(@PathVariable("serviceId") String serviceId,
                          @RequestBody InstanceMetadata instanceMeta) {
         registryService.register(serviceId, instanceMeta);
     }
@@ -47,5 +50,11 @@ public class RegistryController {
     @GetMapping("/snapshot")
     public ServerInstanceSnapshot snapshot() {
         return registryService.generateSnapshot();
+    }
+
+    @PostMapping("/renew")
+    public void renew(@RequestParam("serviceId") String[] serviceId,
+                      @RequestBody InstanceMetadata instanceMeta) {
+        registryService.renew(Arrays.asList(serviceId), instanceMeta);
     }
 }
